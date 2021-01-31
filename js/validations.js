ad410ea78f0a0e1e73b688da7ba88f47e455ed28
CastellDraco <castelldraco@gmail.com>
2021-01-31 12:11:42 -0600

    using innerHTML

--- js/validations.js
+++ js/validations.js
@@ -4,9 +4,16 @@ function Login(evento){
 
     var namefield=document.querySelector("#namefield").value;
     var passfield=document.querySelector("#passfield").value;
+    var feedback=document.querySelector("#user_feedback");
+
+    var message="<p>Welcome "+namefield+" enjoy!</p>";
 
     console.log(namefield);
     console.log(passfield);
+
+    //feedback.textContent="a message without html";
+
+    feedback.innerHTML=message;
 
 }
 
