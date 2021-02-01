5dc21892e5af85df39ca0fe9bbd4efd9fe655aea
CastellDraco <castelldraco@gmail.com>
2021-01-31 19:58:36 -0600

    Random sorting

--- js/arrays.js
+++ js/arrays.js
@@ -4,15 +4,24 @@ var duplicatedcards=cardgroup.concat(cardgroup);
 
 console.log(table);
 
+function randomcards(){
+    var result;
+    //console.log(result);
 
+    result=duplicatedcards.sort(function(){
+        return 0.5-Math.random();
+    });
+    return result;
+}
 
 
 function sortCards(){
     var table=document.querySelector("#table");
-    
+    var cardsrandomized=randomcards();
+
     table.innerHTML="";
 
-    duplicatedcards.forEach(function(element){
+    cardsrandomized.forEach(function(element){
     
         var singlecard=document.createElement("div");
 
